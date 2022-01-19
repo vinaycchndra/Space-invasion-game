3f7e08b3c9a8146e9f487e1876ffc70e8ad717ce
vinaycchndra <85540958+vinaycchndra@users.noreply.github.com>
2022-01-20 00:15:56 +0530

    continuous motion of the ship is added

--- game_functions.py
+++ game_functions.py
@@ -8,14 +8,22 @@ def check_events(ship):
             sys.exit()
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_RIGHT:
-                ship.rect.centerx+=1
+                ship.moving_right = True
 
             elif event.key==pygame.K_LEFT:
-                ship.rect.centerx-=1
+                ship.moving_left = True
+
+        elif event.type==pygame.KEYUP:
+            if event.key==pygame.K_RIGHT:
+                ship.moving_right = False
+                
+            elif event.key==pygame.K_LEFT:
+                ship.moving_left = False
                 
 
 def update_screen(ai_sett,screen,ship):
     screen.fill(ai_sett.bg_color)
+    ship.update()
     ship.blitme()                # Method which plots ship image on the screen
     pygame.display.flip()
     
--- ship.py
+++ ship.py
@@ -9,6 +9,16 @@ class Ship():
         self.screen_rect = self.screen.get_rect()              
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.moving_right = False
+        self.moving_left  = False
+
+    def update(self):
+        if self.moving_right==True:
+            self.rect.centerx+=1
+
+        if self.moving_left==True:                           # Here we are not using the elif condition since it will check only only one true event and 
+            self.rect.centerx-=1                             # it will make ship moving only to the left thus by using both if we can check if both are 
+                                                             # the ship addition and subtraction will happen one after another             
         
     def blitme(self):
         self.screen.blit(self.img,self.rect)                  # Method to place ship image on the screen that was taken as output for this class in the main space_invasion.py file
